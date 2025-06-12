ef19770eea90faf7cda7fd1e7df2699f4a4a533c
Nathan Vervloesem <nathan.vervloesem@kuleuven.be>
2025-06-12 10:22:39 +0200

    MyLayout

--- main.py
+++ main.py
@@ -4,16 +4,26 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 
+
+
+class MyLayout(BoxLayout):
+    def __init__(self):
+        super().__init__()
+        self.button = Button(text='Press me')
+        self.button.bind(on_press=self.new_label)
+
+        self.add_widget(self.button)
+
+    def new_label(self, button):
+        self.label = Label(text='my new label')
+        self.add_widget(self.label)
+        self.remove_widget(button)
+
+
 class MyApp(App):
     def build(self):
-        layout = FloatLayout()
-        label1 = Label(text='Hello World', size_hint=(0.3, 0.2), pos_hint={'center_x':0.2, 'center_y':0.5})
-        label2 = Label(text='Label 2', size_hint=(0.5, 0.7),pos_hint={'center_x':0.5, 'center_y':0.1})
-       
-        layout.add_widget(label1)
-        layout.add_widget(label2)
 
-        return layout
+        return MyLayout()
     
 if __name__ == '__main__':
     MyApp().run()
