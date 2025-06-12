4c7f09a9f1b7ff59c91770a9a0d98af932f6c3a9
Nathan Vervloesem <nathan.vervloesem@kuleuven.be>
2025-06-12 10:11:17 +0200

    FloatLayout

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from kivy.uix.floatlayout import FloatLayout
 
 class MyApp(App):
     def build(self):
-        layout = BoxLayout(orientation='vertical')
-        label1 = Label(text='Hello World')
-        label2 = Label(text='Label 2')
-        button = Button(text='Press Me')
+        layout = FloatLayout()
+        label1 = Label(text='Hello World', size_hint=(0.3, 0.2), pos_hint={'center_x':0.2, 'center_y':0.5})
+        label2 = Label(text='Label 2', size_hint=(0.5, 0.7),pos_hint={'center_x':0.5, 'center_y':0.1})
+       
         layout.add_widget(label1)
         layout.add_widget(label2)
-        layout.add_widget(button)
+
         return layout
     
 if __name__ == '__main__':
