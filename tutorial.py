b1be3960630c0b5c8ac3dcf0cf1795448191a874
Nathan Vervloesem <nathan.vervloesem@kuleuven.be>
2025-06-12 11:13:44 +0200

    Importing images

--- tutorial.py
+++ tutorial.py
@@ -1,6 +1,11 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.uix.image import Image
+from kivy.uix.image import AsyncImage
+
+class MyImage(AsyncImage):
+    pass
 
 class MyBox(Widget):
     myInput = ObjectProperty(None)
@@ -10,7 +15,7 @@ class MyBox(Widget):
 
 class MyApp(App):
     def build(self):
-        return MyBox()
+        return MyImage()
     
 
 if __name__ == '__main__':
