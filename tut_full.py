a896572a23bc86eefa0f461c020d7fc75ce6153a
Nathan Vervloesem <nathan.vervloesem@kuleuven.be>
2025-06-17 09:24:08 +0200

    Add a remove button for the entries in the shoppinglist and make sure to know on which tab one is

--- tut_full.py
+++ tut_full.py
@@ -4,13 +4,39 @@ from kivy.uix.recycleview import RecycleView
 from kivy.properties import ObjectProperty
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.clock import Clock
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.properties import StringProperty
+import pprint
+import requests
 
 
+class SelectableBox(RecycleDataViewBehavior, BoxLayout):
+    text = StringProperty("")
+
+    def on_button_click(self):
+        print('Current tab remove: ',myapp.curr_tab)
+
+        if myapp.curr_tab == 'Lidl':
+            itemlist = myapp.rw.outputcontent1
+        elif myapp.curr_tab == 'Aldi':
+            itemlist = myapp.rw.outputcontent2
+        elif myapp.curr_tab == 'Carrefour':
+            itemlist = myapp.rw.outputcontent3      
+        
+        itemlist.items.remove(self.text)
+        itemlist.update()
+
+        print(f"Button clicked of item: {self.text}")
 class Tabs(TabbedPanel):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
     def on_current_tab(self, instance, value):
-        print("Current tab label:", value.text)
-        return value.text
-    
+         self.curr_tab = value.text
+         print("Current tab label:", value.text)
+         myapp.curr_tab = value.text
+         return value.text
+
 class ListWidget(RecycleView):
 
     def update(self):
@@ -29,43 +55,100 @@ class RootWidget(BoxLayout):
     outputcontent2 = ObjectProperty(None)
     outputcontent3 = ObjectProperty(None)
 
-    def add_item1(self):
-        #print(self.give_current_tab_name())
-        # always prints Default Tab
-        if self.inputcontent1.text != "":
-            # Get correct tab
-            formatted = f'\n -{self.inputcontent1.text}'
-            self.outputcontent1.items.append(formatted)
-            self.outputcontent1.update()
-            self.inputcontent1.text = ""
+    # def get_backend_data(self):
+    #     response = requests.get("http://127.0.0.1:8080/")
+    #     data = response.json()
+    #     print(data) 
+        
 
-    def add_item2(self):
+    # Get the data from the backend - fastapi
+    # # Backend call
+
+    # print(outputcontent1)
+    # outputcontent1.items.append(data["1"])
+    # outputcontent2.items = data["2"]
+    # outputcontent3.items = data["3"]
+    def load_items(self):
+        response = requests.get("http://127.0.0.1:8080/")
+        data = response.json()
+        print(data) 
+
+        self.outputcontent1.items = data["1"]
+        self.outputcontent2.items = data["2"]
+        self.outputcontent3.items = data["3"]
+
+        # update
+        self.outputcontent1.update()
+        self.outputcontent2.update()
+        self.outputcontent3.update()
+
+    def send_to_backend(self):
+        url = "http://127.0.0.1:8080/send"
+        data = {
+            "one": self.outputcontent1.items,
+            "two": self.outputcontent2.items,
+            "three": self.outputcontent3.items
+        }
+        try:
+            response = requests.post(url, json=data)
+            print("Server response:", response.json())
+        except Exception as e:
+            print("Error sending data:", e)
+      
+
+
+    def add_item(self):
         #print(self.give_current_tab_name())
         # always prints Default Tab
-        if self.inputcontent2.text != "":
+        
+        if myapp.curr_tab == 'Lidl':
+            itemlist = self.outputcontent1
+            input = self.inputcontent1
+        elif myapp.curr_tab == 'Aldi':
+            itemlist = self.outputcontent2
+            input = self.inputcontent2
+        elif myapp.curr_tab == 'Carrefour':
+            itemlist = self.outputcontent3 
+            input = self.inputcontent3
+        print('Current tab: ',myapp.curr_tab)
+
+        if input.text != "":
             # Get correct tab
-            formatted = f'\n {self.inputcontent2.text}'
-            self.outputcontent2.items.append(formatted)
-            self.outputcontent2.update()
-            self.inputcontent2.text = ""
+            print( itemlist.items)
+            formatted = f'\n {input.text}'
+            itemlist.items.append(formatted)
+            itemlist.update()
+            input.text = ""
+
+            # and send it to the backend
+            self.send_to_backend()
             
-    def add_item3(self):
-        #print(self.give_current_tab_name())
-        # always prints Default Tab
-        if self.inputcontent3.text != "":
-            # Get correct tab
-            formatted = f'\n -{self.inputcontent3.text}'
-            self.outputcontent3.items.append(formatted)
-            self.outputcontent3.update()
-            self.inputcontent3.text = ""
-
-    # Remove item        
+      
 
 class MyfullApp(App):
+    def __init__(self):
+        super().__init__()
+        self.curr_tab = 'Lidl'
+
     def build(self):
+        rw = RootWidget()
+        self.rw = rw
         tp = Tabs()
         tp.bind(current_tab=tp.on_current_tab)
-        return RootWidget()
-    
+        self.tp = tp
+        self.curr_tab = tp.current_tab
+        self.curr_tab = 'Lidl'
 
-MyfullApp().run()
+        # response = requests.get("http://127.0.0.1:8080/")
+        # data = response.json()
+        # print(data)
+
+        #self.rw.outputcontent1.items = data["1"]
+        #print(self.rw.outputcontent1.items)
+        
+        return rw
+    
+myapp = MyfullApp()
+myapp.build()
+print(myapp.rw.outputcontent1)
+myapp.run()
