d18fb92ab66d0d7f77c2e9ebe050afe20ed6f87e
Nathan Vervloesem <nathan.vervloesem@kuleuven.be>
2025-06-19 11:46:36 +0200

    Adding data_utils file for data operations

---
+++ utils/data_utils.py
@@ -0,0 +1,20 @@
+
+def get_data_difference(list_backend, list_local):
+    diff = []
+    diff_rem_backend = []
+    diff_added_backend = []
+    if list_backend != list_local:
+        for item in list_backend:
+            #print('looping list_backend')
+            if item not in list_local:
+                diff.append(item)
+                diff_added_backend.append(item)
+        for item in list_local:
+            #print('looping list2')
+            if item not in list_backend:
+                diff.append(item)
+                diff_rem_backend.append(item)
+    else:
+        print('Backend and local are identical')
+
+    return diff, diff_rem_backend, diff_added_backend
